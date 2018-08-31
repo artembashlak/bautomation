eaeb727a1605fe10264a2d52182f13d2621eaddd
user <user@MacBook-Pro2231.local>
2018-08-31 17:59:18 +0300

    wdwdw

--- src/test/java/com/waverley/testcases/WikiSearch.java
+++ src/test/java/com/waverley/testcases/WikiSearch.java
@@ -7,28 +7,51 @@ import static org.openqa.selenium.support.ui.ExpectedConditions.textToBePresentI
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.net.URI;
+
 public class WikiSearch {
+
+    private RemoteWebDriver driver;
+
+    @BeforeTest
+    public void openDriver() throws Exception {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("chrome");
+        capabilities.setVersion("latest");
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", false);
+
+        driver = new RemoteWebDriver(URI.create("http://localhost:4444/wd/hub").toURL(), capabilities);
+    }
+
     @Test
-    public void wikiSearchTest() {
-        System.setProperty("webdriver.chrome.driver", "/Users/user/Google Drive/Lectures/lecture_1/com.bashlak/src/test/resources/drivers/chromedriver");
-        // Your test code here. For example:
-        WebDriver driver = new ChromeDriver();
-        WebDriverWait wait = new WebDriverWait(driver, 30);
+    public void wikiSearchTest() throws Exception {
+        Thread.sleep(3000);
         driver.get("https://en.wikipedia.org/wiki/Main_Page");
         By searchInput = By.id("searchInput");
+        WebDriverWait wait = new WebDriverWait(driver, 30);
         wait.until(presenceOfElementLocated(searchInput));
         driver.findElement(searchInput).sendKeys("Software");
         By searchButton = By.id("searchButton");
         wait.until(elementToBeClickable(searchButton));
         driver.findElement(searchButton).click();
-
         wait.until(textToBePresentInElementLocated(By.tagName("body"),
                 "Computer software"));
-        driver.quit();
     }
 
+    @AfterTest
+    public void closeDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
 }
